b9889b9016bf8544ffe196473f0473926331c785
krish4693 <krishnanandunnikrishnan@gmail.com>
2025-01-06 15:12:42 +0530

    layout.tsx dashboard basic structure

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,19 @@
-import React, { Children } from 'react'
+import React from 'react'
+import Sidebar from '@/components/Sidebar/Sidebar'
+import Navbar from '@/components/Navbar/Navbar'
+import Footer from '@/components/Footer/Footer'
+
 
 const layout = ({children,}:Readonly<{
     children:React.ReactNode
 }>) => {
   return (
-    <div>{children}</div>
+    <div>
+        <Sidebar/>
+        <Navbar/>
+        {children}
+        <Footer/>
+    </div>
   )
 }
 
