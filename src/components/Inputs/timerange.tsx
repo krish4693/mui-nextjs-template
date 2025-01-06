e9acd61e8ec0b6579f0630cbcf01dd084c6abe15
jithin.s <jithin.s@armiasystems.com>
2025-01-06 18:23:35 +0530

    added datepicker.time range picker in progress

---
+++ src/components/Inputs/timerange.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import React, { useState } from "react";
+import { TextField } from "@mui/material";
+import dayjs, { Dayjs } from "dayjs";
+export default function DateRange() {
+    const [startTime, setStartTime] = useState<Dayjs | null>(null);
+    const [endTime, setEndTime] = useState<Dayjs | null>(null);
+    const [error, setError] = useState<string | null>(null);
+  
+    const handleFromTimeChange = (newValue: Dayjs | null) => {
+      setStartTime(newValue);
+      if (endTime && newValue && newValue.isAfter(endTime)) {
+        setError('From time cannot be later than To time');
+      } else {
+        setError(null);
+      }
+    };
+  
+    const handleToTimeChange = (newValue: Dayjs | null) => {
+      setEndTime(newValue);
+      if (startTime && newValue && newValue.isBefore(startTime)) {
+        setError('To time cannot be earlier than From time');
+      } else {
+        setError(null);
+      }
+    };
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div>
+        <TimePicker
+          label="From"
+          value={startTime}
+          onChange={handleFromTimeChange}
+        />
+        <TimePicker
+          label="To"
+          value={endTime}
+          onChange={handleToTimeChange}
+        />
+      </div>
+    </LocalizationProvider>
+  );
+}
