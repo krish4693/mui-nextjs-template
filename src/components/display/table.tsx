2422d54758a1944be4f259d2c6d62459e9ede704
jithin.s <jithin.s@armiasystems.com>
2025-01-10 17:10:36 +0530

    added rows per page option and style adjustments

--- src/components/display/table.tsx
+++ src/components/display/table.tsx
@@ -17,9 +17,9 @@ interface Column<T> {
 }
 
 interface TableProps<T> {
-    url: string;
-    // columns: Column<T>[];
-    // data: T[];
+  url: string;
+  // columns: Column<T>[];
+  // data: T[];
 }
 
 export default function CustomTable<T extends Record<string, any>>({
@@ -27,16 +27,25 @@ export default function CustomTable<T extends Record<string, any>>({
 }: TableProps<T>) {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [totalRows, setTotalRows] = useState(0);
   const [tableData, setTableData] = useState<T[]>();
   const [columns, setColumns] = useState<Column<T>[]>();
+
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
   };
 
   const fetchData = async () => {
     let response = await fetch(url);
     let data = await response.json();
     setTableData(data);
+    setTotalRows(data.length);
     setColumns([
       { id: "id", label: "ID" },
       { id: "name", label: "Full Name" },
@@ -52,8 +61,8 @@ export default function CustomTable<T extends Record<string, any>>({
   }, []);
 
   return (
-    <Paper>
-      <TableContainer>
+    <Paper sx={{ width: "100%", overflow: "hidden", marginTop: 8 }}>
+      <TableContainer sx={{ height: 330 }}>
         <Table>
           <TableHead>
             <TableRow>
@@ -63,25 +72,28 @@ export default function CustomTable<T extends Record<string, any>>({
             </TableRow>
           </TableHead>
           <TableBody>
-            {tableData?.map((row) => (
-              <TableRow key={row.id}>
-                {columns?.map((column) => (
-                  <TableCell key={String(column.id)}>
-                    {row[column.id]}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
+            {tableData
+              ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <TableRow key={row.id}>
+                  {columns?.map((column) => (
+                    <TableCell key={String(column.id)}>
+                      {row[column.id]}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
       <TablePagination
         component="div"
-        count={tableData?.length ?? 0}
+        count={totalRows}
         rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[5, 10]}
         page={page}
         onPageChange={handleChangePage}
-        // onRowsPerPageChange={handleChangeRowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
       />
     </Paper>
   );
