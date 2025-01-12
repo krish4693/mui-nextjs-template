d009b101b80b3cb9779bd9fce0c24c3b8033c2b7
Krishnanand U <krishnanandunnikrishnan@gmail.com>
2025-01-12 15:49:22 +0530

    devops: fixes for eslint no-unused-vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // Warns about unused variables
-      "no-unused-vars": ["warn", { args: "none", ignoreRestSiblings: true }],
-    },
-  },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  ...tseslint.configs.recommended,
 ];
-
-export default eslintConfig;
