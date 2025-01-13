1933c1d21062fa63fff3833664368e67568d39dd
Krishnanand U <krishnanandunnikrishnan@gmail.com>
2025-01-13 15:09:36 +0530

    devops: lint rules modfiied

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { 
+    files: ["**/*.{js,mjs,cjs,ts}"], 
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off', // Disable the unused vars rule
+    }
+  },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   ...tseslint.configs.recommended,
 ];
