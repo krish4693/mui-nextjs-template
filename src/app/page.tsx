18a65deaa59e10cc8011960d2cf7aa1c543cb8af
krish4693 <krishnanandunnikrishnan@gmail.com>
2025-01-17 15:39:04 +0530

    ui: darkmode progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,19 @@ import { Container, ThemeProvider, Typography } from "@mui/material";
 import TimeInput from "@/components/Inputs/timepicker";
 import DateRange from "@/components/Inputs/timerange";
 import theme from "@/theme/theme";
+import NightModeToggle from "@/components/NightModeToggle";
 
 export default function Home() {
   return (
     <div>
-      <ThemeProvider theme={theme}>
       <Container sx={{ bgcolor:'primary.main' , height: '100vh',p:'1' }}>
         <Typography variant="h1" sx={{p:1,':hover':{bgcolor:'darkblue'}}}>
           Hello World
+          <NightModeToggle />
         </Typography>
         <TimeInput />
         <DateRange/>
       </Container>
-      </ThemeProvider>
     </div>
   );
 }
--- src/components/NightModeToggle.tsx
+++ src/components/NightModeToggle.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Box, IconButton } from "@mui/material";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
