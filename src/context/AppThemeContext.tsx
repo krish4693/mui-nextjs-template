7aecb7e585b818909f0b2ea46133fc530be47b72
Krishnanand U <krishnanandunnikrishnan@gmail.com>
2025-01-20 14:47:14 +0530

    fix: any type in AppThemeContext, use another type

--- src/context/AppThemeContext.tsx
+++ src/context/AppThemeContext.tsx
@@ -1,62 +1,67 @@
-'use client'
+'use client';
 
-import { createTheme, CssBaseline, responsiveFontSizes, ThemeProvider } from "@mui/material"
-import { createContext, useContext, useMemo } from "react"
+import { createTheme, CssBaseline, responsiveFontSizes, ThemeProvider } from "@mui/material";
+import { createContext, useContext, useMemo, ReactNode } from "react";
 
-import type {} from "@mui/material/themeCssVarsAugmentation"
+import type {} from "@mui/material/themeCssVarsAugmentation";
 
-const AppThemeContext = createContext(null)
+const AppThemeContext = createContext(null);
 
-const AppThemeProvider = (props: any) => {
+type AppThemeProviderProps = {
+  children: ReactNode;
+};
 
-    const theme = useMemo(() => {
-        return responsiveFontSizes(createTheme({
-            cssVariables: {
-                colorSchemeSelector: "class",
-                disableCssColorScheme: true
-            },
+const AppThemeProvider = (props: AppThemeProviderProps) => {
+  const theme = useMemo(() => {
+    return responsiveFontSizes(
+      createTheme({
+        cssVariables: {
+          colorSchemeSelector: "class",
+          disableCssColorScheme: true,
+        },
+        palette: {
+          primary: {
+            main: "rgb(10,18,42)",
+            contrastText: "rgb(255,255,255)",
+          },
+          secondary: {
+            main: "rgb(27,59,111)",
+            contrastText: "rgb(255,255,255)",
+          },
+        },
+        colorSchemes: {
+          light: {
             palette: {
-                primary: {
-                    main: 'rgb(10,18,42)',
-                    contrastText: 'rgb(255,255,255)'
-                },
-                secondary: {
-                    main: 'rgb(27,59,111)',
-                    contrastText: 'rgb(255,255,255)'
-                }
+              primary: {
+                main: "rgb(10,18,42)",
+              },
+              secondary: {
+                main: "rgb(27,59,111)",
+              },
             },
-            colorSchemes: {
-                light: {
-                    palette: {
-                        primary: {
-                            main: 'rgb(10,18,42)',
-                        },
-                        secondary: {
-                            main: 'rgb(27,59,111)',
+          },
+          dark: {
+            palette: {
+              primary: {
+                main: "rgb(10,18,42)",
+              },
+            },
+          },
+        },
+      })
+    );
+  }, []);
 
-                        }
-                    }
-                },
-                dark: {
-                    palette: {
-                        primary: {
-                            main: 'rgb(10,18,42)',
-                        }
-                    }
-                }
-            }
-        }))
-    }, [])
-
-
-    return<AppThemeContext.Provider value={null}>
-        <ThemeProvider  theme={theme} disableTransitionOnChange>
-            <CssBaseline enableColorScheme/>
-            {props.children}
-        </ThemeProvider>
+  return (
+    <AppThemeContext.Provider value={null}>
+      <ThemeProvider theme={theme} disableTransitionOnChange>
+        <CssBaseline enableColorScheme />
+        {props.children}
+      </ThemeProvider>
     </AppThemeContext.Provider>
-}
+  );
+};
 
-export const useAppThemeContext=()=>useContext(AppThemeContext)
+export const useAppThemeContext = () => useContext(AppThemeContext);
 
-export default AppThemeProvider
+export default AppThemeProvider;
