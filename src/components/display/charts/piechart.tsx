a57277a7726d967c9983c591844ebc7326af70f8
Krishnanand U <krishnanandunnikrishnan@gmail.com>
2025-01-26 13:59:35 +0530

    chore: remove Box Variable that cause ESLint issues

--- src/components/display/charts/piechart.tsx
+++ src/components/display/charts/piechart.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
     import { PieChart, pieArcLabelClasses } from "@mui/x-charts/PieChart";
-    import { Box, Paper } from "@mui/material";
+    import { Paper } from "@mui/material";
 
     export default function ChartPie() {
     const desktopOS = [
